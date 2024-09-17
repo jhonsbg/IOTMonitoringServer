e0d40549979b5b8440e5e37dbad25f7abf2cf779
Sebastián Bohórquez Guerrero <jhonsbg@gmail.com>
2024-09-16 23:39:46 -0500

    Change services IPs

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.198.100.30"]
+ALLOWED_HOSTS = ["localhost", "54.174.147.27"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.91.91.147",  # Dirección IP de la base de datos
+        "HOST": "52.91.165.18",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.89.131.47"
+MQTT_HOST = "54.84.215.102"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
